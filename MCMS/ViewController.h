2feedf2d3c3c47190cf9352ba3aab4e92d707d1f
kjmeans <kellemeans@gmail.com>
2014-03-18 19:52:08 -0500

    Initial Commit

---
+++ MCMS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MCMS
+//
+//  Created by Kelle Means on 3/18/14.
+//  Copyright (c) 2014 Meansbiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MCMS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MCMS
+//
+//  Created by Kelle Means on 3/18/14.
+//  Copyright (c) 2014 Meansbiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
